ce7eadfeea0f8f78fe54fd1a18a89d3760be9b7b
bbd33 <damardotnet@gmail.com>
2013-05-26 17:33:52 +0700

    push

---
+++ ClientInformation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BillingClientV3
+{
+    public class ClientInformation : Entities
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Ip { get; set; }
+        public string Group { get; set; }
+    }
+}
---
+++ SessionInformation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BillingClientV3
+{
+    public class SessionInformation : Entities
+    {
+        public int ID { get; set; }
+        public string Code { get; set; }
+        public int Duration { get; set; }
+        public DateTime UpdatedOn { get; set; }
+        public DateTime ExpiredAt { get; set; }
+        public string Text { get; set; }
+        public string StationID { get; set; }
+    }
+}

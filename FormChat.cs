55c429e4d5b9ffee398bfb8ba0b0eb0c4b4dc90c
Putra Budiman <bbd33@github.com>
2013-05-30 08:19:30 +0700

    C

--- FormChat.cs
+++ FormChat.cs
@@ -32,6 +32,7 @@ namespace BillingClientV3
                 clist = rclist.GetData();
                 if(clist != null )
                 {
+
                     //for
                 }
             }
